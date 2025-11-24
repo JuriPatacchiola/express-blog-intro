dda1821ab21a1aea9f2b3ffcf36aff7ed4185447
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-24 12:13:49 +0100

    add the firt array

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = 3000;
 app.use(express.static('public'));
 
 
+const posts = [
+    {
+        titolo: "Ciambellone fatto in casa",
+        contenuto: "Una ricetta semplice e buonissima per preparare il classico ciambellone della nonna.",
+        immagine: "/images/ciambellone.jpeg",
+        tags: ["dolci", "colazione", "tradizione"]
+    },
+]
+
 app.get('/', (req, res) => {
     res.send("Server del mio blog");
 });
