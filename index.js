48de28f5614bff39b26a057a34a6f7e950111957
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-24 12:14:35 +0100

    add all array informaction

--- index.js
+++ index.js
@@ -12,7 +12,31 @@ const posts = [
         immagine: "/images/ciambellone.jpeg",
         tags: ["dolci", "colazione", "tradizione"]
     },
-]
+    {
+        titolo: "Cracker alla barbabietola",
+        contenuto: "Snack croccante e colorato grazie alla barbabietola. Perfetto per un aperitivo sano.",
+        immagine: "/images/cracker_barbabietola.jpeg",
+        tags: ["salato", "snack", "barbabietola"]
+    },
+    {
+        titolo: "Pane fritto dolce",
+        contenuto: "Un dolce povero della tradizione italiana, semplice ma irresistibile.",
+        immagine: "/images/pane_fritto_dolce.jpeg",
+        tags: ["dolci", "fritto", "tradizione"]
+    },
+    {
+        titolo: "Pasta alla barbabietola",
+        contenuto: "Un piatto colorato e sorprendentemente gustoso grazie alla barbabietola.",
+        immagine: "/images/pasta_barbabietola.jpeg",
+        tags: ["pasta", "barbabietola", "primi"]
+    },
+    {
+        titolo: "Torta paesana",
+        contenuto: "Dolce tipico lombardo, fatto con pane raffermo, latte e cacao.",
+        immagine: "/images/torta_paesana.jpeg",
+        tags: ["dolci", "pane", "tradizione"]
+    }
+];
 
 app.get('/', (req, res) => {
     res.send("Server del mio blog");
